15dcb432223b666da5dd6c1fd4d43ed0fd876470
kaugoncalves <kauangoncalves12.ks@gmail.com>
2022-08-02 22:37:44 -0300

    create colors for bad and good stats

--- src/components/pokemonStats.tsx
+++ src/components/pokemonStats.tsx
@@ -6,33 +6,33 @@ export function PokemonStatsView(props?: PokemonStats) {
             <div className=" flex flex-row justify-around">
                 <div className="flex flex-col">
                     <span className="font-bold">HP</span>
-                    <span>{props?.hp}</span>
+                    <span className={`${props && (props.hp < 40) ? 'text-red-600' : props && props.hp > 110 ? 'text-green-600' : ''}`}>{props?.hp}</span>
                 </div>
                 <div className=" flex flex-col">
                     <span className="font-bold">Attack</span>
-                    <span>{props?.attack}</span>
+                    <span className={`${props && (props.attack < 40) ? 'text-red-600' : props && props.attack > 110 ? 'text-green-600' : ''}`}>{props?.attack}</span>
                 </div>
             </div>
 
             <div className="flex flex-row justify-around">
                 <div className="flex flex-col">
                     <span className="font-bold">Defense</span>
-                    <span>{props?.defense}</span>
+                    <span className={`${props && (props.defense < 40) ? 'text-red-600' : props && props.defense > 110 ? 'text-green-600' : ''}`}>{props?.defense}</span>
                 </div>
                 <div className="flex flex-col">
                     <span className="font-bold">Sp. Atk</span>
-                    <span>{props?.specialAttack}</span>
+                    <span className={`${props && (props.specialAttack < 40) ? 'text-red-600' : props && props.specialAttack > 110 ? 'text-green-600' : ''}`}>{props?.specialAttack}</span>
                 </div>
             </div>
 
             <div className="flex flex-row justify-around">
                 <div className="flex flex-col">
                     <span className="font-bold">Sp. Def</span>
-                    <span>{props?.specialDefense}</span>
+                    <span className={`${props && (props.specialDefense < 40) ? 'text-red-600' : props && props.specialDefense > 110 ? 'text-green-600' : ''}`}>{props?.specialDefense}</span>
                 </div>
                 <div className="flex flex-col">
                     <span className="font-bold">Speed</span>
-                    <span>{props?.speed}</span>
+                    <span className={`${props && (props.speed < 40) ? 'text-red-600' : props && props.speed > 110 ? 'text-green-600' : ''}`}>{props?.speed}</span>
                 </div>
             </div>
         </div>
